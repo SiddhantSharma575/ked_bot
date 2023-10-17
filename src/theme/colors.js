cb3eb2a3974a9324a8e2bafcade39417440fdf5a
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-10-17 17:59:00 +0530

    Custom color file added

---
+++ src/theme/colors.js
@@ -0,0 +1,17 @@
+const COLORS = {
+    BACKGROUND: '#ffffff',
+    BACKGROUNDALPH: '#00000099',
+    LIGHTBACKGROUND: '#FFC72C',
+    GRAY: '#F2F5FC',
+    ERROR: '#E30000',
+    SUCCESS: '#d9fae2',
+    TEXTDARK: '#000832',
+    TEXTDARK70: '#000832B3',
+    TEXTDARK50: '#00083280',
+    TEXTDARKPLACEHOLDER: 'rgba(0, 0, 0, 0.50)',
+    TEXTLIGHT: '#000832',
+    PRIMARY: '#FFC72C',
+    BORDER: '#D8DADC',
+  };
+  
+  export default COLORS;
